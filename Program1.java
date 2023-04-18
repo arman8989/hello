c440299496529ca686f4790a31bd9a23d3ae81af
arman8989 <131190518+arman8989@users.noreply.github.com>
2023-04-18 22:22:49 +0530

    Add files via upload
    First program

---
+++ Program1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Program1{
+    public static void main(String args[]){
+        /*
+         here we are calculating the sum of two numbers where i am declaring w as 45 and t 67 so lets see 
+         further
+         */
+        /*int w = 45;
+        int t = 67;
+        int sum = (w+t);
+        System.out.println(sum);
+        Scanner sc = new Scanner(System.in);
+        float rad = sc.nextFloat();
+        float area =  3.14f * rad * rad ;
+        System.out.println(area);
+        */
+        int A = 45;
+        int B = 22;
+        System.out.println("modulo =" + (A%B));
+
+    }
+}
